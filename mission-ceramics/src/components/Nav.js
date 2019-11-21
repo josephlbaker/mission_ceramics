0df617fe6f7a58b32d667a199f71967299ee0dc7
josephlbaker <joelucasbaker@gmail.com>
2019-11-20 19:45:08 -0800

    Made auto spaced grid of images for homepage and styled nav

--- mission-ceramics/src/components/Nav.js
+++ mission-ceramics/src/components/Nav.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import Logo from '../images/logo.png';
 import '../styles/Nav.scss';
 
 export default class Nav extends Component {
   render() {
     return (
       <nav className="nav-container">
+        <img src={Logo} alt="logo" className="logo" />
         <ul className="nav-links">
           <Link to="/"><li>Home</li></Link>
           <Link to="/gallery"><li>Gallery</li></Link>
