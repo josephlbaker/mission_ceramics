79f58d0de2475d07170dfef15de81a8fc4bb252e
josephlbaker <joelucasbaker@gmail.com>
2020-06-08 14:45:12 -0700

    Got rid of cart icon on home screen, added slide transition to nav

--- mission-ceramics/src/components/Nav.js
+++ mission-ceramics/src/components/Nav.js
@@ -10,7 +10,7 @@ export default class Nav extends Component {
 
   componentDidMount() {
     window.addEventListener('scroll', () => {
-      const isTop = window.scrollY < 180;
+      const isTop = window.scrollY < 250;
       if (isTop !== this.state.isTop) {
         this.setState({ isTop })
       }
