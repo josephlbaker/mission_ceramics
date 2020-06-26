85ee7ca5d71663342629c9f43eeceeee3e3ac651
josephlbaker <joelucasbaker@gmail.com>
2020-06-25 17:29:21 -0700

    Readded order api

--- mission-ceramics/src/components/Cart.js
+++ mission-ceramics/src/components/Cart.js
@@ -34,41 +34,11 @@ export default class Cart extends Component {
       }
       line_items.push(line_item);
     }
-    this.createCheckout(line_items);
+    this.createOrder(line_items);
   }
 
-  // createOrder = (line_items) => {
-  //   fetch('https://missionceramics.com/orders', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       order: {
-  //         line_items: line_items
-  //       },
-  //       idempotency_key: uuidv4(),
-  //     })
-  //   })
-  //     .then(response => {
-  //       response.json().then(data => ({
-  //         data: data,
-  //         status: response.status
-  //       })
-  //       ).then(res => {
-  //         this.setState({
-  //           order: res.data.order,
-  //           orderId: res.data.order.id
-  //         })
-  //         this.createCheckout(res.data.order);
-  //       })
-  //     })
-  //     .catch(err => { console.log(err) });
-  // }
-
-  createCheckout = (line_items) => {
-    fetch('https://test-app.missionceramics.com/checkout', {
+  createOrder = (line_items) => {
+    fetch('https://test-app.missionceramics.com/orders', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -76,12 +46,36 @@ export default class Cart extends Component {
       },
       body: JSON.stringify({
         order: {
-          order: {
-            location_id: process.env.REACT_APP_LOCATION_ID,
-            line_items: line_items
-          },
-          idempotency_key: uuidv4()
+          line_items: line_items
         },
+        idempotency_key: uuidv4(),
+      })
+    })
+      .then(response => {
+        response.json().then(data => ({
+          data: data,
+          status: response.status
+        })
+        ).then(res => {
+          this.setState({
+            order: res.data.order,
+            orderId: res.data.order.id
+          })
+          this.createCheckout(res.data.order);
+        })
+      })
+      .catch(err => { console.log(err) });
+  }
+
+  createCheckout = (order) => {
+    fetch('https://test-app.missionceramics.com/checkout', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        order: order,
         idempotency_key: uuidv4(),
         ask_for_shipping_address: true,
         merchant_support_email: "hello@missionceramics.com",
