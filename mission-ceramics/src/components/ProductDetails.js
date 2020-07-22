b302fe62d541a35c4ff1e7eda99661bb4e32d6b8
josephlbaker <joelucasbaker@gmail.com>
2020-07-21 17:28:21 -0700

    Swapped CTA on product details to fixed bottom

--- mission-ceramics/src/components/ProductDetails.js
+++ mission-ceramics/src/components/ProductDetails.js
@@ -4,22 +4,6 @@ import 'font-awesome/css/font-awesome.min.css';
 import closeButton from '../images/x-close-black@2x.png'
 
 const ProductDetails = ({ quantity, setQuantity, addToCart, currentItem, handleClose, showProductDetails, children }) => {
-  let baseWindowHeight = Math.max(window.innerHeight);
-  let classAdded = false;
-  let documentBody = document.body;
-  document.addEventListener('scroll', function (e) {
-    let newWindowHeight = Math.max(window.innerHeight);
-    if (newWindowHeight - 65 > baseWindowHeight) {
-      if (!document.body.classList.contains("ios-toolbar-gone")) {
-        document.body.classList.add("ios-toolbar-gone");
-      }
-    } else {
-      if (document.body.classList.contains("ios-toolbar-gone")) {
-        document.body.classList.remove("ios-toolbar-gone");
-      }
-    }
-  });
-
   const showHideClassName = showProductDetails ? "product-details display-block" : "product-details display-none";
   if (currentItem === null) {
     return null;
@@ -42,14 +26,10 @@ const ProductDetails = ({ quantity, setQuantity, addToCart, currentItem, handleC
                 <option value="5">5</option>
               </select>
             </label>
-
             <div className="add-to-cart-btn-wrapper"><button className="add-to-cart-btn" onClick={addToCart}>add to cart</button></div>
           </div>
-          <div className="close-button-wrapper">
-            <div className="close-button-container">
-              <img src={closeButton} alt="close" className="close-button" />
-            </div>
-            <div className="product-details-spacer"></div>
+          <div className="close-button-container">
+            <img src={closeButton} alt="close" className="close-button" />
           </div>
         </section>
       </div>
